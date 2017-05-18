183baed185bb3de3920df6ec40512df6da439960
Kasia Czajkowska <Kasia Czajkowska>
2017-05-18 14:09:39 +0200

    Zadanie 10.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-function Phone(brand, price, color, year) {
-	this.brand = brand; 
-	this.price = price;
-	this.color = color;
-	this.year = year;
-}
-Phone.prototype.printInfo = function() {
-	console.log(this.year + "," + "The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+function Button(text) {
+	this.text = text || 'Hello';
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver", 2014);
-iPhone6S.printInfo();
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		})
+		$('body').append(this.$element);
+	}
+}
 
-var SamsungGalaxyS6 = new Phone("xxx", 1500, "gold", 2015);
-SamsungGalaxyS6.printInfo();
-
-var OnePlusOne = new Phone ("One", 1000, "white", 2000);
-OnePlusOne.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create();
